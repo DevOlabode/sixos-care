55737616bb37445367d52a9f26f98da2dcf710fa
Samuel Olabode <Solabode499@gmail.com>
2025-09-22 15:35:50 -0600

    Update CSS styles for responsiveness and enhance HTML structure with statistics section on home page

--- routes/sixos.js
+++ routes/sixos.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
+// The HomePage
 router.get('/', (req, res)=>{
     res.render('sixos/home')
 });
 
+// General Services Page
 router.get('/generalServices', (req, res)=>{
     res.render('sixos/generalServices')
 });
 
+//Long Term Services Page
 router.get('/longTerm', (req, res)=>{
     res.render('sixos/longTerm')
 });
